23e64089e7872fbbbc6e44a853823ab33a98d6d7
Eyyyyyyy <103397336+Eyyyyyyy@users.noreply.github.com>
2024-04-11 17:03:23 +0800

    Update Main.java
    取消了search和user的直接联系，可以直接选择search 模式之后搜索

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,41 @@ public class Main {
         db.addBook(new Book("The Great Gatsby"));
         db.addBook(new Book("Mock"));
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        SearchPage p = new SearchPage();
+        String keyword = "";
+        String choice;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("\nEnter either '1' or '2' for search by Title or Author of Book respectively: ");
+            choice = scanner.next();
+
+            if (choice.equals("1") || choice.equals("2"))
+                break;
+            else
+                System.out.println("\nWrong Input!");
+        }
+
+
+        if (choice.equals("1")) {
+            System.out.println("\nEnter the Title of the Book: ");
+            try {
+                keyword = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            p.setSearchStrategy(new TitleSearch());
+            p.searchBook(keyword);
+        } else {
+            System.out.println("\nEnter the Author of the Book: ");
+            try {
+                keyword = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            p.setSearchStrategy(new AuthorSearch());
+            p.searchBook(keyword);
+        }
+
+        
     }
 }
